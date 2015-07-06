b86f9f159d3e356b6a6542ea7de46b699d93a2a4
minjing <min_jing@aliyun.com>
2015-07-06 17:46:02 +0800

    Add RtpSender functionality

---
+++ vlc-java/src/main/java/com/tek/sender/RtpSender.java
@@ -0,0 +1,48 @@
+package com.tek.sender;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+
+public class RtpSender {
+
+    public static void main(String[] args) throws Exception {
+        DatagramSocket client = new DatagramSocket();
+        InetAddress addr = InetAddress.getByName("127.0.0.1");
+        int port = 46688;
+        
+        BufferedReader reader = new BufferedReader(new InputStreamReader(new FileInputStream(
+                new File("E:/repo/vlc-test/vlc-java/rtpPkts.dat"))));
+        String line = reader.readLine();
+        int idx = 0;
+        while(line != null) {
+            idx++;
+            String[] strBytes = line.split(" ");
+            byte[] bytes = new byte[strBytes.length];
+            for (int i = 0; i < strBytes.length; i++) {
+                bytes[i] = convertByte(strBytes[i]);
+            }
+            DatagramPacket pkt = new DatagramPacket(bytes, bytes.length, addr, port);
+            client.send(pkt);
+            System.out.println("Send packate - " + idx + ", size - " + bytes.length);
+            Thread.sleep(20);
+            line = reader.readLine();
+        }
+        reader.close();
+        client.close();
+    }
+    
+    private static byte convertByte(String hexStr) {
+        if (hexStr == null || hexStr.length() != 2) {
+            throw new IllegalArgumentException("The hex string " + hexStr + " can't be converted to byte");
+        }
+        byte b0 = Byte.decode("0x" + hexStr.charAt(0));
+        byte b1 = Byte.decode("0x" + hexStr.charAt(1));
+        b0 = (byte) ((byte) b0 << 4);
+        return (byte) (b0 | b1);
+    }
+}
